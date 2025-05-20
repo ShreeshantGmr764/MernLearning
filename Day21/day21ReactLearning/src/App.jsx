1fd02bd772cd7f576f134b1d00e496c3d90b22d6
ShreeshantGmr764 <withgamergaming764@gmail.com>
2025-05-20 23:00:59 +0545

    Frist commit

---
+++ Day21/day21ReactLearning/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../ecommerce/ecommerce/src/assets/templates/layouts/Layout'
+
+const App = () => {
+  return (
+    <>
+      <Layout/>
+    </>
+  )
+}
+
+export default App
